ffb7ad24d4e2e284a99bbb36e075f9f9aad61979
Scott Chacon <schacon@gmail.com>
2010-12-16 18:12:23 -0800

    reports to generalhawk now

--- lib/snakeeyes.rb
+++ lib/snakeeyes.rb
@@ -1,4 +1,8 @@
-require 'open3'
+require 'rubygems'
+require "json"
+require "uri"
+require 'popen4'
+require "net/http"
 require 'pp'
 
 class SnakeEyes
@@ -8,6 +12,7 @@ class SnakeEyes
   def initialize(path)
     @path = path
     @sleep = 5 * 60 # 5 minutes
+    @sleep = 5
     @debug_level = 99
     @run_count = 0
   end
@@ -25,8 +30,8 @@ class SnakeEyes
 
       if has_new_commits || first_run
         reset_to_newest_commit
-        run_tests
-        report_tests
+        pass, output = run_tests
+        report_tests(pass, output)
       end
 
       sleepy_time
@@ -40,16 +45,31 @@ class SnakeEyes
 
   # if something is new, reset to it
   def reset_to_newest_commit
-    debug "reset to newest commit"
+    debug "reset to newest commit (#{@omaster})"
+    git("reset --hard #{@omaster}")
   end
 
   def run_tests
     debug "run tests"
+    command = git("config cijoe.runner")
+    debug "running '#{command}'...", 1
+    output = ''
+    status = POpen4::popen4(command) do |stdout, stderr, stdin, pid|
+      out = stdout.read
+      err = stderr.read
+      output = out + err
+    end
+    debug "test exitstatus : #{ status.exitstatus }", 2
+    [(status.exitstatus == 0), output]
   end
 
   # report the output to general hawk
-  def report_tests
-    debug "report tests"
+  def report_tests(pass, output)
+    status = pass ? 'good' : 'bad'
+    debug "reporting test results [#{status}] {#{@master}}"
+    data = git('log -1 --format="%s:;:%an" ' + @master)
+    message, author = data.split(":;:")
+    post_results(status, output, message, author, @master)
   end
 
   def has_new_commits
@@ -57,20 +77,20 @@ class SnakeEyes
 
     # look at origin/master branch
     current_master = git("rev-parse origin/master")
-    debug "current o/master #{current_master}", 1
+    debug "current o/master : #{current_master}", 1
 
-    debug "fetching commits"
+    debug "fetching commits", 1
     git('fetch')
 
     # look at origin/master branch again
     new_master = git("rev-parse origin/master")
-    debug "new o/master #{new_master}", 1
+    debug "new o/master     : #{new_master}", 1
 
     # set master branch SHA internally
     @omaster = new_master
 
     @master = git("rev-parse refs/heads/master")
-    debug "master #{@master}", 1
+    debug "master           : #{@master}", 1
 
     # return true if they differ
     new_master != current_master
@@ -85,7 +105,7 @@ class SnakeEyes
 
   def git(command)
     out = ''
-    Open3.popen3("git #{command}") do |stdin, stdout, stderr|
+    status = POpen4::popen4("git #{command}") do |stdout, stderr, stdin, pid|
       out = stdout.read
     end
     out.chomp
@@ -96,5 +116,55 @@ class SnakeEyes
       tabs = "\t" * level
       puts tabs + message 
     end
+  end
+
+  ## General Hawk Stuff ##
+  
+  def post_results(status, output, message, author, sha)
+    config = hawk_config
+    data = {
+        "agent"       => config[:agent],
+        "description" => config[:description],
+        "branch"      => "master",
+        "author"      => author,
+        "sha"         => sha,
+        "status"      => status,
+        "url"         => config[:url],
+        "message"     => message,
+        "output"      => output
+      }
+    post_update(data.to_json) # POST JSON TO URL
+  end
+
+  def hawk_config
+    return @config if @config
+    c = {}
+    config = git('config --list')
+    config.split("\n").each do |line| 
+      k, v = line.split('=')
+      c[k] = v
+    end
+    url = ''
+    u = c['remote.origin.url']
+    if m = /github\.com.(.*?)\/(.*?)\.git/.match(u)
+      user = m[1]
+      proj = m[2]
+      url = "https://github.com/#{user}/#{proj}"
+    end
+
+    @config = {
+      :server    => c['hawk.server'],
+      :token     => c['hawk.token'],
+      :agent     => c['hawk.agent'],
+      :description => c['hawk.description'],
+      :url => url
+    }
+  end
+
+  def post_update(data)
+    config = hawk_config
+    ws = "#{config[:server]}/update/#{config[:token]}"
+    x = Net::HTTP.post_form(URI.parse(ws), {'data' => data})
+    pp x
   end
 end
